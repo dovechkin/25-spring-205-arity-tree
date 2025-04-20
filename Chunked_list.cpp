b8ffd76c2a8cd57ff22143300c0613d1f32fc89b
dovechkin <deovechkin@gmail.com>
2025-04-21 00:45:46 +0300

    new

--- Chunked_list.cpp
+++ Chunked_list.cpp
@@ -78,6 +78,97 @@ template <size_t chunk_size, typename T> class chunked_list
         tail->data[tail->current_size++] = value;
         ++total_size;
     }
+
+    void insert(size_t index, const T &value)
+    {
+        if (index > total_size) {
+            throw std::out_of_range("Index out of range");
+        }
+        if (index == total_size) {
+            push_back(value);
+        } else {
+            size_t numchunk = index / chunk_size;
+            size_t index_in_chunk = index % chunk_size;
+            Chunk *current = head;
+            for (int i = 0; i < numchunk; ++i) {
+                current = current->next;
+            }
+            if (index_in_chunk > current->current_size) {
+                current->data[index_in_chunk] = value;
+                current->current_size += 1;
+            } else if (current->current_size < chunk_size) {
+                if (current->current_size < total_size)
+                    for (int i = index_in_chunk; i < current->current_size;
+                         ++i) {
+                        current->data[i + 1] = current->data[i];
+                        current->current_size += 1;
+                    }
+                current->data[index_in_chunk] = value;
+                current->current_size += 1;
+            } else {
+                Chunk *new_chunk = new Chunk();
+                size_t elements_to_move =
+                    current->current_size - index_in_chunk;
+
+                for (size_t i = 0; i < elements_to_move; ++i) {
+                    new_chunk->data[i] = current->data[index_in_chunk + i];
+                    new_chunk->current_size++;
+                }
+                current->current_size = index_in_chunk;
+
+                current->data[current->current_size++] = value;
+                total_size++;
+
+                new_chunk->next = current->next;
+                current->next = new_chunk;
+
+                if (current == tail) {
+                    tail = new_chunk;
+                }
+            }
+        }
+    }
+    void erase(size_t index)
+    {
+        if (index >= total_size) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        size_t numchunk = index / chunk_size;
+        size_t index_in_chunk = index % chunk_size;
+        Chunk *current = head;
+        for (int i = 0; i < numchunk; ++i) {
+            current = current->next;
+        }
+
+        for (size_t i = index_in_chunk; i < current->current_size - 1; ++i) {
+            current->data[i] = current->data[i + 1];
+        }
+        current->current_size--;
+        total_size--;
+
+        if (current->current_size == 0) {
+            Chunk *prev = nullptr;
+            Chunk *tmp = head;
+            while (tmp != current) {
+                prev = tmp;
+                tmp = tmp->next;
+            }
+
+            if (prev) {
+                prev->next = current->next;
+            } else {
+                head = current->next;
+            }
+
+            if (current == tail) {
+                tail = prev;
+            }
+
+            delete current;
+        }
+    }
+
     size_t size() const { return total_size; }
     T &operator[](size_t index)
     {
@@ -110,32 +201,39 @@ template <size_t chunk_size, typename T> class chunked_list
 
 int main()
 {
-    // Создаем список с чанками по 3 элемента
     chunked_list<3, int> numbers;
 
-    // Добавляем элементы (больше, чем размер чанка)
     for (int i = 0; i < 10; ++i) {
         numbers.push_back(i * 10);
     }
 
-    // Проверяем размер списка
     std::cout << "Total elements: " << numbers.size() << "\n\n"; // 10
 
-    // Выводим элементы через индекс
     std::cout << "Elements: ";
     for (size_t i = 0; i < numbers.size(); ++i) {
         std::cout << numbers[i] << " "; // 0 10 20 30 40 50 60 70 80 90
     }
     std::cout << "\n\n";
 
-    // Тест копирования
+    numbers.erase(3);
+
+    numbers.insert(1, 30);
+    numbers.insert(10, 100);
+
+    std::cout << "Total elements: " << numbers.size() << "\n\n"; // 10
+
+    std::cout << "Elements: ";
+    for (size_t i = 0; i < numbers.size(); ++i) {
+        std::cout << numbers[i] << " "; // 0 10 20 30 40 50 60 70 80 90
+    }
+    std::cout << "\n\n";
+
     chunked_list<3, int> numbers_copy = numbers;
     numbers_copy.push_back(100);
     std::cout << "Copied list last element: " << numbers_copy[10]
               << "\n"; // 100
     std::cout << "Original list size: " << numbers.size() << "\n\n"; // 10
 
-    // Тест с другим типом данных
     chunked_list<2, std::string> words;
     words.push_back("Hello");
     words.push_back("World");
@@ -144,14 +242,13 @@ int main()
 
     std::cout << "Strings: ";
     for (size_t i = 0; i < words.size(); ++i) {
-        std::cout << words[i] << " "; // Hello World Chunked List
+        std::cout << words[i] << " ";
     }
     std::cout << "\n\n";
 
-    // Тест исключения
     try {
         std::cout << "Trying to access index 100: ";
-        std::cout << numbers[100] << "\n"; // Исключение
+        std::cout << numbers[100] << "\n";
     } catch (const std::out_of_range &e) {
         std::cout << "Error: " << e.what() << "\n";
     }
