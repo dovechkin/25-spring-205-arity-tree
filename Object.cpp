09bc20d993c0464b9a0a2b053a492b4d0f6fa5e4
dovechkin <deovechkin@gmail.com>
2025-04-21 10:27:13 +0300

    new

---
+++ Object.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class Object
+{
+  private:
+    std::unordered_map<std::string, std::string> map_;
+
+  public:
+    void insert(std::string key, std::string value)
+    {
+        map_[std::move(key)] = std::move(value);
+    }
+
+    std::string get(const std::string &key) const
+    {
+        auto it = map_.find(key);
+        return (it != map_.end()) ? it->second : "";
+    }
+};
+
+int main()
+{
+    Object obj;
+    obj.insert("name", "Alice");
+    obj.insert("age", "30");
+
+    std::cout << obj.get("name") << " "; // Alice
+    std::cout << obj.get("age") << " "; // 30
+    std::cout << obj.get("city") << "end" << std::endl; // (пустая строка)
+}
